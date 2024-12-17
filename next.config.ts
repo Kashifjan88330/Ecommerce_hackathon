d0d7d0873e3b35fe8741f62617209d348cb2cfa4
kashif jan <kj236680@gmail.com>
2024-12-17 14:29:07 +0500

    final code

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,9 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+// next.config.js
+module.exports = {
+  images: {
+    domains: ['s3-alpha-sig.figma.com', 'via.placeholder.com'], // Add any other image sources you're using here
+  },
+};
